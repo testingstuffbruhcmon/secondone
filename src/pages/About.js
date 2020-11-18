2bf8d038869b169b3e21f4e33b368a5d535366a6
Chris <chrispyc@gmail.com>
2020-11-18 04:14:38 +0400

    stuff

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import coverImg from '../img/home1.png';
 
 const About = ()=>{
 
 
     return (
         <StyledAbout>
-            About
+            <h1>About section</h1>
+            <div className="imgTop">
+            <img src={coverImg} alt="camera image"/>
+            </div>
         </StyledAbout>
     )
 }
 
 var StyledAbout = styled.div`
-    background-color:yellow;
-    color: ${props => props.main ? 'green' : 'cyan'}
+    background-color:rgb(70,70,70);
+    font-size:5rem;
+    display:flex;
+    justify-content:space-between;
+    height:90vh;
+
+    & > *{
+        flex:1
+        
+    }
+
+    & .imgTop{ 
+        border:1px solid blue;
+        padding:15vh 0;
+    
+     & img{
+        display:block;
+        width:auto;
+        height:70vh;
+        object-fit:contain;
+
+      }
+    }
 `;
 
 
